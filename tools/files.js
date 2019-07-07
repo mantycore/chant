efe8c5cc5110722c04869a64b78bfed0c8d45ec4
Mantycore <mantycore@gmail.com>
2019-07-08 00:03:04 +0300

    mkdirp warning

--- tools/files.js
+++ tools/files.js
@@ -3,8 +3,10 @@ const fs = require('fs')
 const util = require('util')
 
 async function writeAttachment({cid, name, type, buffer}) {
-    const path = `content/${type}/${cid.substring(2, 4)}` //maybe find a way to save type in the path
-    shell.mkdir('-p', path)
+    //const sanitizedType = type.replace(/[^-a-zA-Z0-9.;+/]/g, '~')
+    // TODO check what characters are allowed in media type and what are safe to use here
+    const path = `content/${type}/${cid.substring(2, 4)}`
+    shell.mkdir('-p', path) // replace with mkdirp?
     await util.promisify(fs.writeFile)(`${path}/${cid}.${name}`, buffer)
 }
 
